16de702d6b7f4df9725e674809e1a8dc36593150
Mihonos <imihailovic530@gmail.com>
2023-06-23 18:32:02 +0200

    added and changed tests because of nightmare selectors on this page

--- src/test/java/page/LoekHomePageRedirections.java
+++ src/test/java/page/LoekHomePageRedirections.java
@@ -1,9 +1,13 @@
 package page;
 
 import base.BaseTest;
+import org.junit.Assert;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class LoekHomePageRedirections extends BaseTest {
 
@@ -15,16 +19,61 @@ public class LoekHomePageRedirections extends BaseTest {
     WebElement logoBtn;
     @FindBy(xpath = "//ul[@id='top-menu']/li[1]")
     WebElement aboutUsBtn;
+    @FindBy(xpath = "//ul[@id='top-menu']/li[2]")
+    WebElement pricesBtn;
+    @FindBy(xpath = "//ul[@id='top-menu']/li[3]")
+    WebElement contactBtn;
+    @FindBy(xpath = "//ul[@id='top-menu']/li[4]")
+    WebElement loginBtn;
+    @FindBy(xpath = "//ul[@id='top-menu']/li[5]/a")
+    WebElement regiterBtn;
+    @FindBy(className = "et_pb_promo_button")
+    WebElement registerNowBtn;
+    @FindBy(xpath = "//a[@href='https://loekonline.nl/over-ons/'][contains(.,'over LOEK!')]")
+    WebElement overLoekBtn;
+
 
     public String logoBtnLeadsToHomePage(){
         logoBtn.click();
         return driver.getCurrentUrl();
     }
-
     public String aboutUsBtnLeadsToOverOnsPage(){
         aboutUsBtn.click();
         return driver.getCurrentUrl();
     }
-
+    public String pricesBtnLeadsToPricesPage(){
+        pricesBtn.click();
+        return driver.getCurrentUrl();
+    }
+    public String contactBtnleadsToContactPage(){
+        contactBtn.click();
+        return driver.getCurrentUrl();
+    }
+    public String loginBtnleadsToLoginPage(){
+        Actions actions = new Actions(driver);
+        actions.moveToElement(loginBtn).build().perform();
+        loginBtn.click();
+        return driver.getCurrentUrl();
+    }
+    public String registerBtnleadsToRegisterPage(){
+        regiterBtn.click();
+        return driver.getCurrentUrl();
+    }
+    public void registerNowBtnleadsToRegisterPage(){
+        //Actions actions = new Actions(driver);
+        //actions.moveToElement(registerNowBtn).build().perform();
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", registerNowBtn);
+        wdWait.until(ExpectedConditions.elementToBeClickable(registerNowBtn));
+        registerNowBtn.click();
+        String URL = driver.getCurrentUrl();
+        Assert.assertEquals(URL, "https://app.loekonline.nl/user/register");
+    }
+    public void overLoekBtnleadsToOverOnsPage() {
+        //((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", overLoekBtn);
+        //wdWait.until(ExpectedConditions.elementToBeClickable(overLoekBtn));
+        overLoekBtn.click();
+        String URL = driver.getCurrentUrl();
+        Assert.assertEquals(URL, "https://loekonline.nl/over-ons/");
+    }
 
 }
--- src/test/java/test/HeaderBtnsRedirectionTest.java
+++ src/test/java/test/HeaderBtnsRedirectionTest.java
@@ -13,21 +13,49 @@ public class HeaderBtnsRedirectionTest extends BaseTest {
     @Before
     public void SetUpTest(){
         loekHomePageRedirections = new LoekHomePageRedirections();
-        loekHomePageRedirections.logoBtnLeadsToHomePage();
-        loekHomePageRedirections.aboutUsBtnLeadsToOverOnsPage();
-        //loekHomePageRedirections.pricesBtnLeadsToPricesPage();
-        //loekHomePageRedirections.contactBtnleadsToContactPage();
-        //loekHomePageRedirections.loginBtnleadsToLoginPage();
     }
-
 
     @Test
-    public void HeaderBtnsRedirectionTest() {
+    public void LogoBtnRedirectionTest() {
+        loekHomePageRedirections.logoBtnLeadsToHomePage();
         Assert.assertEquals("https://loekonline.nl/", loekHomePageRedirections.logoBtnLeadsToHomePage());
-        Assert.assertEquals("https://loekonline.nl/over-ons/", loekHomePageRedirections.aboutUsBtnLeadsToOverOnsPage());
-        //Assert.assertEquals("https://loekonline.nl/prijzen/", loekHomePageRedirections.pricesBtnLeadsToPricesPage());
-        //Assert.assertEquals("https://loekonline.nl/contact/", loekHomePageRedirections.contactBtnleadsToContactPage());
-        //Assert.assertEquals("https://app.loekonline.nl/user/login", loekHomePageRedirections.loginBtnleadsToLoginPage());
-
     }
+    @Test
+    public void AboutUsBtnRedirectionTest() {
+        loekHomePageRedirections.aboutUsBtnLeadsToOverOnsPage();
+        Assert.assertEquals("https://loekonline.nl/over-ons/", loekHomePageRedirections.aboutUsBtnLeadsToOverOnsPage());
+    }
+    @Test
+    public void PricesBtnRedirectionTest() {
+        loekHomePageRedirections.pricesBtnLeadsToPricesPage();
+        Assert.assertEquals("https://loekonline.nl/prijzen/", loekHomePageRedirections.pricesBtnLeadsToPricesPage());
+    }
+    @Test
+    public void ContactBtnRedirectionTest() {
+        loekHomePageRedirections.contactBtnleadsToContactPage();
+        Assert.assertEquals("https://loekonline.nl/contact/", loekHomePageRedirections.contactBtnleadsToContactPage());
+    }
+    @Test
+    public void LoginBtnRedirectionTest() {
+        loekHomePageRedirections.loginBtnleadsToLoginPage();
+        Assert.assertEquals("https://app.loekonline.nl/user/login", loekHomePageRedirections.loginBtnleadsToLoginPage());
+    }
+    @Test
+    public void RegisterBtnRedirectionTest() {
+        loekHomePageRedirections.registerBtnleadsToRegisterPage();
+        Assert.assertEquals("https://app.loekonline.nl/user/register", loekHomePageRedirections.registerBtnleadsToRegisterPage());
+    }
+    @Test
+    public void RegisterNowBtnRedirectionTest() {
+        loekHomePageRedirections.registerNowBtnleadsToRegisterPage();
+        //Assert.assertEquals("https://app.loekonline.nl/user/register", loekHomePageRedirections.registerNowBtnleadsToRegisterPage());
+    }
+    @Test
+    public void OverLoekBtnRedirectionTest() {
+        loekHomePageRedirections.overLoekBtnleadsToOverOnsPage();
+        //Assert.assertEquals("https://loekonline.nl/over-ons/", loekHomePageRedirections.overLoekBtnleadsToOverOnsPage());
+    }
+
+
+
 }
